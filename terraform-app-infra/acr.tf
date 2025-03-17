53206fc7226358e76133df4d65e57aa36935ea01
as2024ad <183369419+as2024ad@users.noreply.github.com>
2025-03-17 10:22:57 +0000

    Change ACR SKU to Premium

--- terraform-app-infra/acr.tf
+++ terraform-app-infra/acr.tf
@@ -2,7 +2,7 @@ resource "azurerm_container_registry" "acr" {
   name                = "acr${var.project_name}${var.instance}"
   resource_group_name = azurerm_resource_group.webapp_rg.name
   location            = azurerm_resource_group.webapp_rg.location
-  sku                 = "Basic"
+  sku                 = "Premium"
   admin_enabled       = false
   public_network_access_enabled = false
 }
