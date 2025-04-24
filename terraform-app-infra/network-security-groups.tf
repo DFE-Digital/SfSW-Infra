f004666558eef4a8a52360f957f878da8d335e8f
as2024ad <183369419+as2024ad@users.noreply.github.com>
2025-04-24 16:31:45 +0100

    Remove NSG rules for app_service_nsg

--- terraform-app-infra/network-security-groups.tf
+++ terraform-app-infra/network-security-groups.tf
@@ -34,34 +34,40 @@ resource "azurerm_subnet_network_security_group_association" "app_gateway_subnet
   network_security_group_id = azurerm_network_security_group.app_gateway_nsg.id
 }
 
+
+
+
+
 # NSG for App Service
 resource "azurerm_network_security_group" "app_service_nsg" {
   name                = "nsg-app-service-${var.project_name}-${var.instance}"
   location            = azurerm_resource_group.webapp_rg.location
   resource_group_name = azurerm_resource_group.webapp_rg.name
 
-  security_rule {
-    name                       = "Github-Runners"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefixes    = ["0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0"] # placeholders for GitHub Runner IPs
-    destination_address_prefix = var.app_service_snet
-  }
-  security_rule {
-    name                         = "Contentful-CMS"
-    priority                     = 100
-    direction                    = "Outbound"
-    access                       = "Allow"
-    protocol                     = "Tcp"
-    source_port_range            = "*"
-    destination_port_range       = "*"
-    source_address_prefix        = var.app_service_snet
-    destination_address_prefixes = ["0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0"] # placeholders for Contentful IPs
-  }
+  # security_rule {
+  #   name                       = "Github-Runners"
+  #   priority                   = 100
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "*"
+  #   source_address_prefixes    = ["0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0"] # placeholders for GitHub Runner IPs
+  #   destination_address_prefix = var.app_service_snet
+  # }
+  # security_rule {
+  #   name                         = "Contentful-CMS"
+  #   priority                     = 100
+  #   direction                    = "Outbound"
+  #   access                       = "Allow"
+  #   protocol                     = "Tcp"
+  #   source_port_range            = "*"
+  #   destination_port_range       = "*"
+  #   source_address_prefix        = var.app_service_snet
+  #   destination_address_prefixes = ["0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0"] # placeholders for Contentful IPs
+  # }
+
+  
 }
 
 resource "azurerm_subnet_network_security_group_association" "app_service_subnet_nsg_association" {
@@ -92,6 +98,17 @@ resource "azurerm_subnet_network_security_group_association" "app_service_delega
   subnet_id                 = azurerm_subnet.app_service_delegated_subnet.id
   network_security_group_id = azurerm_network_security_group.app_service_delegated_nsg.id
 }
+
+
+
+
+
+
+
+
+
+
+
 
 # NSG for Private Endpoints
 resource "azurerm_network_security_group" "private_endpoints_nsg" {
