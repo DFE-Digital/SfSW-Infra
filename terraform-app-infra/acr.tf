65f40450038d2afc088695455990d50245469b80
as2024ad <183369419+as2024ad@users.noreply.github.com>
2025-05-13 15:24:16 +0100

    principal_type for ACR

--- terraform-app-infra/acr.tf
+++ terraform-app-infra/acr.tf
@@ -20,4 +20,5 @@ resource "azurerm_role_assignment" "mi_acr_pull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_user_assigned_identity.mi_app_service.principal_id
+  principal_type       = "ServicePrincipal"
 }
