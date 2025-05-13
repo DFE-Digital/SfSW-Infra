4edd71f5d0aaf6fa77acf0d04f7016518861180e
as2024ad <183369419+as2024ad@users.noreply.github.com>
2025-05-13 14:05:29 +0100

    ACR admin_enabled

--- terraform-app-infra/acr.tf
+++ terraform-app-infra/acr.tf
@@ -3,7 +3,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.webapp_rg.name
   location            = azurerm_resource_group.webapp_rg.location
   sku                 = "Premium"
-  admin_enabled       = true
+  admin_enabled       = false
   public_network_access_enabled = false
   lifecycle {
     ignore_changes = [
