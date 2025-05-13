5e4fdd8a9bd9405d95af7c4b500689c770a2ae2d
as2024ad <183369419+as2024ad@users.noreply.github.com>
2025-05-13 10:30:14 +0100

    New KV

--- terraform-app-infra/key-vault.tf
+++ terraform-app-infra/key-vault.tf
@@ -1,5 +1,5 @@
 data "azurerm_key_vault" "key_vault" {
-  name                = "kv-${var.project_name}-${var.instance}-temp-2"
+  name                = "kv-${var.project_name}-${var.instance}-temp-3"
   resource_group_name = data.azurerm_resource_group.core_infra_rg.name
 }
           # ------------------------------------------
