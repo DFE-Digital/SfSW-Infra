264850d689f5138b26f353c90a9276475fc879af
as2024ad <183369419+as2024ad@users.noreply.github.com>
2025-05-16 16:49:04 +0100

    Test MI destroy

--- terraform-core-infra/managed-identity.tf
+++ terraform-core-infra/managed-identity.tf
@@ -2,10 +2,16 @@ resource "azurerm_user_assigned_identity" "mi_app_service" {
   name                = "uai-app-service-${var.project_name}-${var.instance}"
   location            = azurerm_resource_group.core_infra_rg.location
   resource_group_name = azurerm_resource_group.core_infra_rg.name
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_user_assigned_identity" "mi_appgw" {
   name                = "uai-appgw-${var.project_name}-${var.instance}"
   location            = azurerm_resource_group.core_infra_rg.location
   resource_group_name = azurerm_resource_group.core_infra_rg.name
+  lifecycle {
+    prevent_destroy = true
+  }
 }
