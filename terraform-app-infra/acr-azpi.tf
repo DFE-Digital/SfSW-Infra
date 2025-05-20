21edae6505e1d775fd3403c6a43f4fe2bc5aef0a
as2024ad <183369419+as2024ad@users.noreply.github.com>
2025-05-20 16:52:12 +0100

    -

--- terraform-app-infra/acr-azpi.tf
+++ terraform-app-infra/acr-azpi.tf
@@ -1,6 +1,6 @@
 resource "azapi_resource" "acr_identity_config" {
   type      = "Microsoft.Web/sites/config@2022-03-01"
-  name      = "${azurerm_linux_web_app.app_service.name}/web"
+  name      = "web"
   parent_id = azurerm_linux_web_app.app_service.id
   body = {
     properties = {
