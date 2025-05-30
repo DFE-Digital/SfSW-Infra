880450929c163d40c20ca5fcb78b15b2a98392ed
as2024ad <183369419+as2024ad@users.noreply.github.com>
2025-05-30 14:29:57 +0100

    -

--- terraform-app-infra/monitoring.tf
+++ terraform-app-infra/monitoring.tf
@@ -3,7 +3,7 @@ resource "azurerm_log_analytics_workspace" "log_analytics_ws" {
   location                   = azurerm_resource_group.webapp_rg.location
   resource_group_name        = azurerm_resource_group.webapp_rg.name
   sku                        = "PerGB2018"
-  internet_ingestion_enabled = false
+  internet_ingestion_enabled = true
   internet_query_enabled     = true
   retention_in_days = 30
 }
@@ -14,7 +14,7 @@ resource "azurerm_application_insights" "app_insights_web" {
   resource_group_name = azurerm_resource_group.webapp_rg.name
   application_type    = "web"
   disable_ip_masking = true
-  internet_ingestion_enabled = false
+  internet_ingestion_enabled = true
   internet_query_enabled = true
 }
 
