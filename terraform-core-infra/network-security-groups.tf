b66ddf3424007efdcdbe0eac902e1d23b17b2f5a
as2024ad <183369419+as2024ad@users.noreply.github.com>
2025-06-11 11:49:29 +0100

    -

--- terraform-core-infra/network-security-groups.tf
+++ terraform-core-infra/network-security-groups.tf
@@ -1,33 +1,33 @@
 # NSG for Application Gateway
-# resource "azurerm_network_security_group" "app_gateway_nsg" {
-#   name                = "nsg-app-gateway-${var.project_name}-${var.instance}"
-#   location            = azurerm_resource_group.core_infra_rg.location
-#   resource_group_name = azurerm_resource_group.core_infra_rg.name
+resource "azurerm_network_security_group" "app_gateway_nsg" {
+  name                = "nsg-app-gateway-${var.project_name}-${var.instance}"
+  location            = azurerm_resource_group.core_infra_rg.location
+  resource_group_name = azurerm_resource_group.core_infra_rg.name
 
-#   security_rule {
-#     name                       = "HTTP-HTTPS"
-#     priority                   = 100
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_ranges    = ["80", "443"]
-#     source_address_prefix      = "Internet"
-#     destination_address_prefix = var.app_gateway_snet
-#   }
+  security_rule {
+    name                       = "HTTP-HTTPS"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["80", "443"]
+    source_address_prefix      = "Internet"
+    destination_address_prefix = var.app_gateway_snet
+  }
 
-#   security_rule {
-#     name                       = "Gateway-Manager"
-#     priority                   = 110
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_ranges    = ["65200-65535"]
-#     source_address_prefix      = "Internet"
-#     destination_address_prefix = var.app_gateway_snet
-#   }
-# }
+  security_rule {
+    name                       = "Gateway-Manager"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["65200-65535"]
+    source_address_prefix      = "Internet"
+    destination_address_prefix = var.app_gateway_snet
+  }
+}
 
 resource "azurerm_subnet_network_security_group_association" "app_gateway_subnet_nsg_association" {
   subnet_id                 = azurerm_subnet.app_gateway_subnet.id
