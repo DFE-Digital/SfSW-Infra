40968d3b5f780c7404aa7303636295428b9e9609
Matt Barton <matt.barton@hippodigital.co.uk>
2025-11-13 12:16:31 +0000

    fix: created role assignment to allow app service to access blob storage

--- terraform-app-infra/managed-identity.tf
+++ terraform-app-infra/managed-identity.tf
@@ -7,3 +7,9 @@ data "azurerm_user_assigned_identity" "mi_appgw" {
   name                = "uai-appgw-${var.project_name}-${var.instance}"
   resource_group_name = data.azurerm_resource_group.core_infra_rg.name
 }
+
+resource "azurerm_role_assignment" "mi_app_service_sa_access" {
+  scope = azurerm_storage_account.error_page_sa
+  principal_id = data.azurerm_user_assigned_identity.mi_app_service
+  role_definition_name = "Storage Blob Data Contributor"
+}
