99da676b9f49e151d7c41e652403eccdd3b368b0
Bob Lawson <armand.lawson@education.gov.uk>
2025-11-27 15:09:11 +0000

    fix: remove managed identity role assignment

--- terraform-app-infra/managed-identity.tf
+++ terraform-app-infra/managed-identity.tf
@@ -7,10 +7,3 @@ data "azurerm_user_assigned_identity" "mi_appgw" {
   name                = "uai-appgw-${var.project_name}-${var.instance}"
   resource_group_name = data.azurerm_resource_group.core_infra_rg.name
 }
-
-resource "azurerm_role_assignment" "mi_app_service_sa_access" {
-  scope                = azurerm_storage_account.error_page_sa.id
-  principal_type       = "ServicePrincipal"
-  principal_id         = data.azurerm_user_assigned_identity.mi_app_service.principal_id
-  role_definition_name = "Storage Blob Data Contributor"
-}
