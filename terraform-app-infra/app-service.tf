0f32c2ebff464a1654f68dc0f2fee953cb887d3f
Bob Lawson <armand.lawson@education.gov.uk>
2025-12-02 14:22:51 +0000

    fix: Add config to staging slot

--- terraform-app-infra/app-service.tf
+++ terraform-app-infra/app-service.tf
@@ -113,6 +113,8 @@ resource "azurerm_linux_web_app_slot" "staging" {
     DOCKER_ENABLE_CI                            = "false"
     WEBSITES_ENABLE_APP_SERVICE_STORAGE         = "false"
     CPD_AZURE_STORAGE_ACCOUNT_CONNECTION_STRING = "@Microsoft.KeyVault(VaultName=${data.azurerm_key_vault.key_vault.name};SecretName=DataProtectionStorageConnection)"
+    CPD_DATA_PROTECTION_KEY                     = azurerm_key_vault_key.data_protection_key.id
+    AZURE_CLIENT_ID                             = data.azurerm_user_assigned_identity.mi_app_service.client_id
   }
 
   site_config {
