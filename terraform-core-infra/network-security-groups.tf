452a8c913c17a152743be0366980b3933f54ee9d
Bob Lawson <armand.lawson@education.gov.uk>
2025-12-09 16:33:09 +0000

    fix: Only add nsg rule to prod

--- terraform-core-infra/network-security-groups.tf
+++ terraform-core-infra/network-security-groups.tf
@@ -4,16 +4,20 @@ resource "azurerm_network_security_group" "app_gateway_nsg" {
   location            = azurerm_resource_group.core_infra_rg.location
   resource_group_name = azurerm_resource_group.core_infra_rg.name
 
-  security_rule {
-    name                       = "HTTP-HTTPS"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_ranges    = ["80", "443"]
-    source_address_prefix      = "Internet"
-    destination_address_prefix = var.app_gateway_snet
+  # conditional rule for production only
+  dynamic "security_rule" {
+    for_each = var.environment == "production" ? [1] : [0]
+    content {
+      name                       = "HTTP-HTTPS"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_ranges    = ["80", "443"]
+      source_address_prefix      = "Internet"
+      destination_address_prefix = var.app_gateway_snet
+    }
   }
 
   security_rule {
