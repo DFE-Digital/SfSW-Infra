e53eff6a6ee410db7a6eab0e9f89c3ce91dc1fa5
Bob Lawson <armand.lawson@education.gov.uk>
2026-02-04 12:11:10 +0000

    fix: Only allow HTTPS on web app

--- terraform-app-infra/app-service.tf
+++ terraform-app-infra/app-service.tf
@@ -17,6 +17,7 @@ resource "azurerm_linux_web_app" "app_service" {
   resource_group_name = azurerm_resource_group.webapp_rg.name
   location            = azurerm_service_plan.app_service_plan.location
   service_plan_id     = azurerm_service_plan.app_service_plan.id
+  https_only          = true
 
   public_network_access_enabled   = false
   key_vault_reference_identity_id = data.azurerm_user_assigned_identity.mi_app_service.id
